508f6904fd5570afec1c0bf8e2ccbe2bbf11d207
Ankit Mani Pandey <ankitpandey78600@gmail.com>
2026-03-15 02:08:50 +0530

    redefined file structure

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const connectDB = require('./database/config'); // Import your DB function
+const connectDB = require('../database/config'); // Import your DB function
 
 const app = express();
 
