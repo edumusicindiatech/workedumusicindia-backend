d4adcc9e6bab06e6b00363ee72e006a6c3419198
Ankit Mani Pandey <ankitpandey78600@gmail.com>
2026-04-07 18:11:38 +0530

    fixed the express json upload limit

--- src/server/server.js
+++ src/server/server.js
@@ -91,7 +91,8 @@ app.use(cors({
     ],
     credentials: true
 }));
-app.use(express.json());
+app.use(express.json({ limit: '30mb' })); 
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
 
 app.use('/api/auth', authRouter);
 app.use('/api/admin', adminRouter);
