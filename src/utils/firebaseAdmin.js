5200aa4b8c9eab536ec9c737fe5df556caba0b32
Ankit Mani Pandey <ankitpandey78600@gmail.com>
2026-04-18 18:18:12 +0530

    some changes in firebaseAdmin.js

--- src/utils/firebaseAdmin.js
+++ src/utils/firebaseAdmin.js
@@ -1,5 +1,14 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../../firebase-service-account.json');
+
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+    // Production (Render): Parse the JSON string from the environment variable
+    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+    // Local Development: Require the physical file
+    serviceAccount = require('../../firebase-service-account.json');
+}
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
