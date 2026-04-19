7a2dfc42dda2186754d38d9d327a04d49bd6dc32
Ankit Mani Pandey <ankitpandey78600@gmail.com>
2026-04-19 19:43:03 +0530

    ota update

--- src/server/server.js
+++ src/server/server.js
@@ -354,6 +354,7 @@ app.use(cors({
 
 app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
+app.use(express.static('public'));
 
 app.use('/api/auth', authRouter);
 app.use('/api/admin', adminRouter);
